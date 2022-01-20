fdfdb7c127b29814f96e842b068063bbd2146fc3
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-01-20 15:48:34 +0530

    testing done

--- app/src/main/java/com/example/ed_talk/LoginActivity.java
+++ app/src/main/java/com/example/ed_talk/LoginActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //first try Done
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
