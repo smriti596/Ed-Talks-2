5baca39f7e75cfa713b5ecd270a37419eaa8cf9e
Smriti596 <psmriti71@gmail.com>
2022-01-23 17:34:50 +0530

    Modified RegisterActivity and LoginActivity

--- app/src/main/java/com/example/ed_talk/LoginActivity.java
+++ app/src/main/java/com/example/ed_talk/LoginActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView register,forgotPassword;
@@ -100,11 +101,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 if(task.isSuccessful()) {
                     //redirect
                     //displaying success message temporarily
-                    Toast.makeText(LoginActivity.this,"Successfully logged in you FUCKIN' DICKHEAD",Toast.LENGTH_LONG).show();
-                    progressBar.setVisibility((View.GONE));
+
+                    FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+                    if(user.isEmailVerified()){
+                        //redirect-temporarily displayed toast message.
+                        Toast.makeText(LoginActivity.this,"Successfully logged in you FUCKIN' DICKHEAD",Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility((View.GONE));
+                    }else{
+                        user.sendEmailVerification();
+                        Toast.makeText(LoginActivity.this,"Email not verified! Check your mail box and Try Again!",Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility((View.GONE));
+                    }
+
                 }else
                 {
-                    Toast.makeText(LoginActivity.this,"Failed to login! Please check your credentials.",Toast.LENGTH_LONG).show();
+                    //Toast.makeText(LoginActivity.this,"Failed to login! Please check your credentials.",Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this,"Failed to login!"+task.getException().getMessage(),Toast.LENGTH_LONG).show();
                     progressBar.setVisibility((View.GONE));
                 }
             }
