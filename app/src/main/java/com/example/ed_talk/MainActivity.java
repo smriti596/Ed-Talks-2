f1168eb61dc615275447fcaef9608c4c5cf37f62
Smriti596 <psmriti71@gmail.com>
2022-03-19 22:22:32 +0530

    Navigation Menu

--- app/src/main/java/com/example/ed_talk/MainActivity.java
+++ app/src/main/java/com/example/ed_talk/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home,R.id.brochureFragment,R.id.websiteFragment,R.id.developerFragment,R.id.nav_gallery, R.id.nav_slideshow,R.id.bookmarksFragment,R.id.qnaFragment)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
