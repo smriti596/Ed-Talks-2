4e5fb7cef00f9e34d9d93f5d9710f6999453918b
Vaishnavi <vaishnaviagnihotri40@gmail.com>
2022-04-06 10:50:35 +0530

    name and email id on navigation bar top

--- app/src/main/java/com/example/ed_talk/MainActivity.java
+++ app/src/main/java/com/example/ed_talk/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.ed_talk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.ed_talk.Utils.SharedPrefManager;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -13,13 +17,20 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.ed_talk.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    View mHeaderView;
+    private FirebaseAuth mAuth;
+    TextView mUserName,mUserEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +41,19 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.appBarMain.toolbar);
 
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+       drawer = binding.drawerLayout;
+      navigationView = binding.navView;
+
+
+
+       // else {
+       //     startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        //    finish();
+       // }
+
+
+
+        // Passing each menu ID as a set of Ids because each
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -41,6 +63,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        SharedPrefManager sharedPrefManager=new SharedPrefManager(this);
+        if(sharedPrefManager.isLoggedIn()){
+            updateSideNavHeader();
+        }
     }
 
     @Override
@@ -55,5 +81,28 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void updateSideNavHeader(){
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mHeaderView =  mNavigationView.getHeaderView(0);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // View
+        mUserName = (TextView) mHeaderView.findViewById(R.id.name);
+        mUserEmail = (TextView) mHeaderView.findViewById(R.id.email);
+        //mUserImage = (ImageView) mHeaderView.findViewById(R.id.imageView);
+
+        // Set username & email
+
+        mUserName.setText(mAuth.getCurrentUser().getDisplayName());
+        mUserEmail.setText(mAuth.getCurrentUser().getEmail());
+       /* Glide.with(getApplicationContext())
+                .load(mAuth.getCurrentUser().getPhotoUrl())
+                .apply(new RequestOptions()
+                        .placeholder(R.mipmap.ic_launcher)
+                        .fitCenter())
+                .into(mUserImage);*/
     }
 }
