1197fc387ac144671d0d5339ee5523ae06a99c82
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-04-06 12:52:59 +0530

    Complete AddInterviewExperienceActivity.java and realtime database also getting updated

--- app/src/main/java/com/example/ed_talk/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ed_talk/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.ed_talk.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.ed_talk.AddInterviewExperience.AddInterviewExperienceActivity;
 import com.example.ed_talk.R;
 import com.example.ed_talk.databinding.FragmentHomeBinding;
 
@@ -19,6 +22,9 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
+    LinearLayout shareInterviewExperience;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +34,15 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
+        shareInterviewExperience = (LinearLayout)root.findViewById(R.id.shareInterviewExperience);
+        shareInterviewExperience.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(), AddInterviewExperienceActivity.class);
+                startActivity(i);
+            }
+        });
 
         return root;
     }
